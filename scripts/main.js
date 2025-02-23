3cc414e6a1cb08f8dc068742b34228cfe4c320fb
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-02-23 20:15:42 +0200

    feat: organise project files and create contact page

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const messageButton = document.getElementById("contactButtonName");
+messageButton.addEventListener("click", () => {
+    window.open("../pages/messageSent.html", "_blank"); });
+
+
+
